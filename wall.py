067be4db0853e1658e1888cebc840c0a2e38d844
owerko <tomek@bytespack.com>
2023-01-19 15:18:21 +0100

    add walls psets

--- wall.py
+++ wall.py
@@ -1,7 +1,13 @@
 import ifcopenshell.util.element
+import pprint as pp
 
 m = ifcopenshell.open("Duplex_A.ifc")
 walls = m.by_type("IfcWall")
 print(len(walls))
 
 print(f'Liczba ścian w modelu: {len(walls)}')
+
+walls = m.by_type("IfcWall")
+wall = walls[1]
+pset_for_wall = ifcopenshell.util.element.get_psets(wall)
+pp.pprint(pset_for_wall)
