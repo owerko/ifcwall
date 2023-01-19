868c70cc99113ff334ea56b4686f03988136eab0
owerko <tomek@bytespack.com>
2023-01-19 15:29:48 +0100

    add externall walls count

--- wall.py
+++ wall.py
@@ -8,6 +8,11 @@ print(len(walls))
 print(f'Liczba ścian w modelu: {len(walls)}')
 
 walls = m.by_type("IfcWall")
-wall = walls[1]
-pset_for_wall = ifcopenshell.util.element.get_psets(wall)
-pp.pprint(pset_for_wall)
+ext_walls = []
+for w in walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    if psets.get("Pset_WallCommon"):
+        if bool(psets.get("Pset_WallCommon").get("IsExternal")):
+            ext_walls.append(w)
+            
+print(f'Liczba ścian zewnętrznych: {len(ext_walls)}')
